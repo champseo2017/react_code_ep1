a78c24135b2f4821a095339bf94fbfff58f8316c
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-09-04 17:57:01 +0700

    init project 01

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    name: "Lion",
+    scientificName: "Panthero leo",
+    size: 140,
+    diet: ["meat"],
+  },
+  {
+    size: 205,
+    diet: ["plants", "insects"],
+    additional: {
+      notes:
+        "This is the eastern gorilla. There is also a west  that is a different species.",
+    },
+    name: "Gorilla",
+    scientificName: "Gorilla beringei",
+  },
+  {
+    size: 322,
+    diet: ["plants"],
+    additional: {
+      notes: "There are three different species of zebra.",
+      link: "https://en.wikipedia.org/wiki/Zebra",
+    },
+    name: "Zebra",
+    scientificName: "Equus quagga",
+  },
+];
